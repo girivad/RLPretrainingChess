6e07f793132b969a92c7d77b069153e722826ead
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-02-09 23:38:18 -0800

    Don't use argparse, new config for training 8ntp transformers

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 pip install torch numpy transformers datasets tiktoken wandb tqdm --no-cache-dir -q
 # python data/lichess_hf_dataset/prepare.py --dataset lichess_6gb_blocks.zip --out_dir ../../model_vol/data_dir/pretrain/
-python train.py config/train_shakespeare_char.py --model_dir ../../model_vol --data_dir ../../model_vol/data_dir/pretrain/ --run_name 8layer_ntp_lichess
+python train.py config/train_8ntp_char.py
