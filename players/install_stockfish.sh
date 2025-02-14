062595594449690c1c19ca225b7a86187379a6d5
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-02-13 23:48:41 -0800

    Install Stockfish Executable

---
+++ players/install_stockfish.sh
@@ -0,0 +1,4 @@
+wget https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64.tar
+tar xvf stockfish-ubuntu-x86-64.tar
+mv stockfish/stockfish-ubuntu-x86-64 stockfish_exec
+chmod +x stockfish_exec
