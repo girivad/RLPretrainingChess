d7a229017ac6c12bd8aaeabdea649861aafc84e4
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-02-13 16:47:45 -0800

    Allow train configuration

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+train_config=$1
 nvidia-smi
 pip install torch numpy transformers datasets tiktoken wandb tqdm --no-cache-dir -q
 # python data/lichess_hf_dataset/prepare.py --dataset lichess_6gb_blocks.zip --out_dir ../../model_vol/data_dir/pretrain/
-python train.py config/train_8ntp_char.py
+python train.py $train_config
