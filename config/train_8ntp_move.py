093168f5de20400ebd09d5e1e126728e16aa8b0f
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-03-02 23:32:22 -0800

    Pretraining Configurations for Seer-Forcing and Next-Token Prediction

---
+++ config/train_8ntp_move.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+model_dir = "../../model_vol"
+data_dir = "../../model_vol/data_dir/pretrain"
+
+timestamp = datetime.now(ZoneInfo("America/Los_Angeles"))
+run_name = "2-6layer_moves_mtp_lichess " + str(timestamp)
+
+out_dir = os.path.join(model_dir, run_name)
+eval_interval = 4000
+eval_iters = 100
+ckpt_interval = 50000
+# I'm not sure what's going on, but when log_interval == 100, the time per iter is inaccurate and much longer than it should be
+# when running on multiple GPUs. TODO: investigate
+log_interval = 50  # don't print too too often
+
+always_save_checkpoint = True
+
+wandb_log = True
+wandb_project = "chessformer"
+wandb_run_name = run_name
+
+dataset = data_dir
+gradient_accumulation_steps = 1
+batch_size = 100
+block_size = 1023  # context of up to 1023 tokens (because dataset block size is 1024)
+
+# baby GPT model :)
+n_slayer = 2
+n_layer = 6
+n_head = 8
+n_embd = 512
+dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
+bias = False # do we use bias inside LayerNorm and Linear layers?
+vocab_size = 1970
+
+# aux losses
+aux_seer_loss = True
+
+learning_rate = 3e-4
+max_iters = 600000
+lr_decay_iters = max_iters  # make equal to max_iters usually
+min_lr = 3e-5  # learning_rate / 10 usually
+beta2 = 0.95  # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 2000  # not super necessary potentially
+compile = True
