9112f7dae28383346852421f657135818b184cda
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-03-11 09:25:15 +0000

    Automatically install BayesElo

--- RL.sh
+++ RL.sh
@@ -3,5 +3,6 @@ train_config=$1
 nvidia-smi
 pip install torch numpy transformers datasets tiktoken wandb tqdm tzdata chess --no-cache-dir -q
 bash players/install_stockfish.sh
+bash setup_bayeselo.sh
 num_procs=$(python -c "import torch; print(torch.cuda.device_count());")
 torchrun --nproc_per_node=${num_procs} RLtrain.py $train_config
---
+++ setup_bayeselo.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/ddugovic/BayesianElo.git
+cd BayesianElo/src
+make
+cd ../../
