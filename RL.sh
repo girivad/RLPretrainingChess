9647cbe12dc16f5034b4d7315b433c8d759aac24
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-03-12 10:55:19 -0700

    Create dir for game transcripts

--- RL.sh
+++ RL.sh
@@ -5,4 +5,5 @@ pip install torch numpy transformers datasets tiktoken wandb tqdm tzdata chess -
 bash players/install_stockfish.sh
 bash setup_bayeselo.sh
 num_procs=$(python -c "import torch; print(torch.cuda.device_count());")
+mkdir pgn
 torchrun --nproc_per_node=${num_procs} RLtrain.py $train_config
