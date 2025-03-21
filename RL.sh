c6bb86b91c0104c2110d7491ef3845165980498b
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-03-21 13:46:13 -0700

    Perform NVIDIA_SMI post setup

--- RL.sh
+++ RL.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 train_config=$1
-nvidia-smi
 pip install torch numpy transformers datasets tiktoken wandb tqdm tzdata chess --no-cache-dir -q
 bash players/install_stockfish.sh
 bash setup_bayeselo.sh
@@ -8,4 +7,5 @@ mkdir openings
 wget -O openings/eco_openings.pgn https://storage.googleapis.com/searchless_chess/data/eco_openings.pgn 
 num_procs=$(python -c "import torch; print(torch.cuda.device_count());")
 mkdir pgn
+nvidia-smi
 torchrun --nproc_per_node=${num_procs} RLtrain.py $train_config
