5090abe467e5454958aef31decb8c9efca84794e
Giridhar Vadhul <gvadhul@ucsc.edu>
2025-03-24 22:48:27 -0700

    Setup dependencies for evaluation

--- train.sh
+++ train.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 train_config=$1
-nvidia-smi
-pip install torch numpy transformers datasets tiktoken wandb tqdm tzdata --no-cache-dir -q
+pip install torch numpy transformers datasets tiktoken wandb tqdm tzdata chess --no-cache-dir -q
+bash players/install_stockfish.sh
+bash setup_bayeselo.sh
+mkdir openings
+wget -O openings/eco_openings.pgn https://storage.googleapis.com/searchless_chess/data/eco_openings.pgn 
 num_procs=$(python -c "import torch; print(torch.cuda.device_count());")
+mkdir pgn
+nvidia-smi
 torchrun --nproc_per_node=${num_procs} train.py $train_config
